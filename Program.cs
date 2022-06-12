d6cefc5f8b7143377c042bd321d637f1478979bc
ArielMejia <kevinmejia_2010@hotmail.com>
2022-06-12 17:44:43 -0600

    Creating a new console app fron cli

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Humanizer;
+
+Console.WriteLine("Ingrese su nombre");
+var nombre = Console.ReadLine();
+Console.WriteLine("Ingrese su edad");
+var edad = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Hola, mi nombre es: " + nombre + " Y tengo: " + edad.ToWords(new System.Globalization.CultureInfo("es")) + " años");
+
